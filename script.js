8f4c65d1bedeb02d436c73b6d71a46f255d57a24
Dallin <dallin.johansen@kombea.com>
2023-10-19 11:49:48 -0600

    Added functionality to play video on scroll and auto pause

--- script.js
+++ script.js
@@ -1,47 +1,19 @@
-// Function to fetch and display images
-function loadImages() {
-    const imageContainer = document.getElementById('content');
+const video = document.getElementById("elon_video");
+const videoContainer = document.querySelector(".elon_overlay");
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.6, // Trigger when 60% of the video container is visible
+};
 
-    // Replace 'images/' with the path to your images folder
-    const imagesFolder = 'images/';
-
-    // Array of image filenames
-    const imageFiles = [
-        'Slide1.jpg',
-        'Slide2.jpg',
-        'Slide3.jpg',
-        'Slide4.jpg',
-        'Slide5.jpg',
-        'Slide6.jpg',
-        'Slide7.jpg',
-        'Slide8.jpg',
-        'Slide9.jpg',
-        'Slide10.jpg',
-        'Slide11.jpg',
-        'Slide12.jpg',
-        'Slide13.jpg',
-        'Slide14.jpg',
-        'Slide15.jpg',
-        'Slide16.jpg',
-        'Slide17.jpg',
-        'Slide18.jpg',
-        'Slide19.jpg',
-        'Slide20.jpg',
-        'Slide21.jpg',
-    ];
-
-    imageFiles.forEach((filename) => {
-        const img = document.createElement('img');
-        img.src = imagesFolder + filename;
-        img.alt = filename;
-        
-        const flexItem = document.createElement('div');
-        flexItem.classList.add('flex-item');
-        flexItem.appendChild(img);
-
-        imageContainer.appendChild(flexItem);
+const videoObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            video.play();
+        } else {
+            video.pause();
+        }
     });
-}
+}, options);
 
-// Call the loadImages function when the page loads
-window.addEventListener('load', loadImages);
+videoObserver.observe(videoContainer);
