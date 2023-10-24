b657fa813a86af2756a94c6b442c16b0b1751328
Dallin Johansen <dallin.johansen@kombea.com>
2023-10-24 13:17:45 -0600

    .

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ window.addEventListener('scroll', function() {
         header.classList.remove('scrolled');
     }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const audioBoxes = document.querySelectorAll(".audio-item");
+
+    audioBoxes.forEach((box) => {
+        const audioId = box.getAttribute("data-audio");
+        const audioElement = new Audio(audioId);
+
+        box.addEventListener("click", function () {
+            if (audioElement.paused) {
+                audioElement.play();
+            } else {
+                audioElement.pause();
+                //audioElement.currentTime = 0;
+            }
+        });
+    });
+});
+
