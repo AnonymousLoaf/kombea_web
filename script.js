a2358fac5f6f5533f79bb46d4444980608bd7cfa
Dallin Johansen <dallin.johansen@kombea.com>
2023-10-24 11:39:32 -0600

    Updated web

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-const video = document.getElementById("elon_video");
-const videoContainer = document.querySelector(".elon_overlay");
-const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.6, // Trigger when 60% of the video container is visible
-};
-
-const videoObserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            video.play();
-        } else {
-            video.pause();
-        }
-    });
-}, options);
-
-videoObserver.observe(videoContainer);
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.site-header');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
