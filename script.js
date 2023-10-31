ca51f9a524ab3e0860958cf89eae5850d711d0af
Dallin Johansen <dallin.johansen@kombea.com>
2023-10-31 13:29:05 -0600

    .

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const navLinks = document.querySelectorAll('.site-nav a');
-
+// This changes the header color on scroll and the text for the links
 document.addEventListener('scroll', function() {
     const header = document.querySelector('.site-header');
+    const navLinks = document.querySelectorAll('.site-nav a');
 
     navLinks.forEach((link) => {
         if (window.scrollY > 0) {
@@ -14,7 +14,7 @@ document.addEventListener('scroll', function() {
     });
 });
 
-
+// This is for the audio links on the four boxes
 document.addEventListener("DOMContentLoaded", function () {
     const audioBoxes = document.querySelectorAll(".audio-item");
 
@@ -33,6 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+// This is the scroll function for the links in the header
 document.addEventListener("DOMContentLoaded", function () {
     // Select all links inside the site navigation
     const navLinks = document.querySelectorAll(".site-header a");
@@ -56,11 +57,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelectorAll(".site-nav a");
+    let isScrolling = false; // Variable to track scrolling
+
+    window.addEventListener("scroll", function () {
+        if (!isScrolling) {
+            const scrollPosition = window.scrollY;
+
+            navLinks.forEach((link) => {
+                const targetId = link.getAttribute("href").substring(1);
+                const targetSection = document.getElementById(targetId);
+
+                if (targetSection) {
+                    const sectionTop = targetSection.offsetTop;
+                    const sectionBottom = sectionTop + targetSection.clientHeight;
+
+                    if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+                        // Remove the "active" class from all links
+                        navLinks.forEach((navLink) => {
+                            navLink.classList.remove("active");
+                        });
+
+                        // Add the "active" class to the current section's link
+                        link.classList.add("active");
+                    }
+                }
+            });
+        }
+    });
 
     // Add a click event listener to each link
     navLinks.forEach((link) => {
         link.addEventListener("click", function (e) {
             e.preventDefault();
+
+            // Disable scrolling while handling the click event
+            isScrolling = true;
+
             const targetId = this.getAttribute("href").substring(1);
             const targetElement = document.getElementById(targetId);
 
@@ -73,41 +105,23 @@ document.addEventListener("DOMContentLoaded", function () {
                 });
 
                 // Add the "active" class to the clicked link
-                link.classList.add("active");
-            }
-        });
-    });
+                this.classList.add("active");
 
-    window.addEventListener("scroll", function () {
-        const scrollPosition = window.scrollY;
-
-        navLinks.forEach((link) => {
-            const targetId = link.getAttribute("href").substring(1);
-            const targetSection = document.getElementById(targetId);
-
-            if (targetSection) {
-                const sectionTop = targetSection.offsetTop;
-                const sectionBottom = sectionTop + targetSection.clientHeight;
-
-                if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-                    // Remove the "active" class from all links
-                    navLinks.forEach((navLink) => {
-                        navLink.classList.remove("active");
-                    });
-
-                    // Add the "active" class to the current section's link
-                    link.classList.add("active");
-                }
+                // Re-enable scrolling after a short delay (adjust as needed)
+                setTimeout(function () {
+                    isScrolling = false;
+                }, 800); // 500 milliseconds, adjust as needed
             }
         });
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const mobileMenu = document.getElementById("mobile-menu");
-    const nav = document.querySelector(".site-nav ul");
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const siteNav = document.querySelector('.site-nav');
 
-    mobileMenu.addEventListener("click", function() {
-        nav.classList.toggle("show");
+    // Add a click event listener to the mobile menu icon
+    mobileMenu.addEventListener('click', function () {
+        siteNav.classList.toggle('open'); // Toggle the 'open' class
     });
 });
